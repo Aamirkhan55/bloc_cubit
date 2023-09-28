e77a807685b59e611fc7fd29b81716f36512456d
Amir55143 <aamirnoor55143@gmail.com>
2023-09-28 13:03:45 +0500

    BlocBulider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_cubit/cubit/counter_cubit.dart';
 import 'package:bloc_cubit/ui/screens/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,17 +12,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, 
-      title: 'Bloc Cubit',
-      theme: ThemeData(
-        primaryColor: Colors.deepPurple,
-        useMaterial3: true,
-        appBarTheme:  AppBarTheme(
-          backgroundColor: Colors.deepPurpleAccent.shade100,
-        )
+    return BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Bloc Cubit',
+        theme: ThemeData(
+            primaryColor: Colors.deepPurple,
+            useMaterial3: true,
+            appBarTheme: AppBarTheme(
+              backgroundColor: Colors.deepPurpleAccent.shade100,
+            )),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_cubit/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,26 +12,42 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         title: const Text('Bloc Cubit'),
       ),
-      body: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-           Center(
-            child: Text('This is Cubit'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocBuilder<CounterCubit, int>(
+              builder: (context, state) {
+                return Text(
+                  "$state",
+                  style: const TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 10),
+            const Text('Click the counter to see the magic'),
+          ],
+        ),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: (){},
+            onPressed: () {
+              BlocProvider.of<CounterCubit>(context).decrement();
+            },
             child: const Icon(Icons.remove),
-            ),
+          ),
           const SizedBox(height: 10),
           FloatingActionButton(
-            onPressed: (){},
+            onPressed: () {
+              BlocProvider.of<CounterCubit>(context).increment();
+            },
             child: const Icon(Icons.add),
-            ),
+          ),
         ],
       ),
     );
