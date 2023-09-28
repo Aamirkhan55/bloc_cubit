d2aeb6411fe3db60deaeaf04411f8ec4da1ba744
Amir55143 <aamirnoor55143@gmail.com>
2023-09-28 12:52:24 +0500

    Method/Icrement/Decrement

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,8 +1,11 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<int> {
   CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+
+  void decrement() => emit(state - 1);
 }
